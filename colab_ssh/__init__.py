be9e01d6799e664a35e92da94c0b36d0d4431a7b
divyam234 <divyam@mnnit.ac.in>
2023-09-01 15:59:02 +0530

    update

--- colab_ssh/__init__.py
+++ colab_ssh/__init__.py
@@ -3,7 +3,6 @@ from colab_ssh.init_git import init_git, init_git_cloudflared
 from colab_ssh._command import run_command, run_with_pipe
 from colab_ssh.launch_direct_ssh import launch_direct_ssh
 from colab_ssh.set_private_key import set_private_key
-from colab_ssh.get_tunnel_config import get_tunnel_config
 from colab_ssh.launch_ssh_cloudflared import launch_ssh_cloudflared
 
 __all__ = [
@@ -13,6 +12,5 @@ __all__ = [
     "run_with_pipe",
     "launch_direct_ssh",
     "set_private_key",
-    "get_tunnel_config",
     "launch_ssh_cloudflared",
 ]
