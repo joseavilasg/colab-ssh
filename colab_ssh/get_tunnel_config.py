5691160147631c181e1e576c02569e6edc0c8388
José Avila SG <joseavilsg.dev@gmail.com>
2023-12-18 21:39:17 -0500

    Fix getting correct hostname when the tunnel restarts

--- colab_ssh/get_tunnel_config.py
+++ colab_ssh/get_tunnel_config.py
@@ -5,13 +5,13 @@ def get_argo_tunnel_config(initial_path=""):
   hostname = None
   with open(initial_path+"cloudflared.log", "r") as f:
     output = "".join(f.readlines())
-  result = re.search(
+  results = re.findall(
       ':\"\| *https://(.+?.trycloudflare.com) *\|\"}', output)
 
-  if result is None:
+  if not results:
     raise Exception(
         "Cannot get any result from cloudflared metrics")
-  hostname = result.group(1)
+  hostname = results[-1]
   if hostname is None:
     raise Exception(
         "Cannot get the hostname from cloudflared, it looks like the connection has failed.")
